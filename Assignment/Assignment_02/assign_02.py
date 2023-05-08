41e5218261066aac94c87e13c185c6778ac6629a
Hoangkim <hoangkimlong1301@gmail.com>
2023-05-08 17:22:05 +0200

    update

--- Assignment/Assignment_02/assign_02.py
+++ Assignment/Assignment_02/assign_02.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import re
+from fractions import Fraction
+
 
 def readFile(file_name):
     with open(file_name,'r') as file:
@@ -32,7 +34,7 @@ def convert_lable(df):
 
 def create_count_and_probability(file_name):
     # here comes your code
-    words = []
+    word_vector = []
     with open(file_name,'r') as file:
         # iterate over each line of the file
         for line in file:
@@ -41,28 +43,54 @@ def create_count_and_probability(file_name):
             # add each word to the list if it doesn't already exist in the list
             for word in line_words:
                 word = word.replace('?','').replace('.','').lower()
-                if word not in words:
-                    words.append(word)  
+                if word not in word_vector:
+                    word_vector.append(word)  
     # print the list of words
-    print(words)
-    #for line in file:
+    print(word_vector)
+   
+    count_Vector=[]
+    with open(file_name,'r') as file:
+        for line in file:
+            vector_temp = countVectorize(line,word_vector)   
+            count_Vector.append(vector_temp)
+    print(count_Vector)
+    
+
+    cal = []
+    with open(file_name,'r') as file:
+        for line in file:
+            temp = []
+            line_words = line.split()
+            lw = []
+            for word in line_words:
+                word = word.replace('?','').replace('.','').lower()
+                lw.append(word)
+            for word in lw:
+                word = word.replace('?','').replace('.','').lower()
+                temp.append(Fraction(lw.count(word), len(lw)))
+            cal.append(temp)
         
-    
-    
-    
-    
-    
+    for p in cal:
+        print(p)
     #return(csv_file)
     
     
-def countVectorize(line,vector):   
-    
-    
-    
-    
-    
-    
-    return()
+def countVectorize(string,vector):   
+    #create a list to store vector
+    count = []
+    #split string and store it to a list
+    split_string = string.split()
+    #for loop
+    #go through each word in the given vector
+    for word in vector:
+        count_temp = 0
+        for loStr in split_string:
+            loStr = loStr.replace('?','').replace('.','').lower()
+            if word == loStr:
+                count_temp+=1
+        count.append(count_temp)
+        
+    return(count)
                  
             
 if __name__ == "__main__":
@@ -70,12 +98,8 @@ if __name__ == "__main__":
     #print(df['Label'])
     #convert_lable(df)
     
-    
     create_count_and_probability('/Users/tommy/Documents/Informatik_Uni_Marburg/Natural_Language_Processing/Assignment/Assignment_02/corpus.txt')
-    my_string = "this is the first document"
-
-    # count the occurrences of the word "the" in the string
-    count = my_string.count("the")
-
-    # print the count
-    print(count)
+    
+    countVectorize('This document is the second document',['this', 'is', 'the', 'first', 'document', 'second', 'and', 'third', 'one'])
+    
+    
